85b6f6c648f6f59ae0295effb04cc827c7cf2160
johnnylindbergh <johnnyl8@embarqmail.com>
2017-08-06 19:25:23 -0400

    name change

--- src/Autocomplete/Main.java
+++ src/Autocomplete/Main.java
@@ -1,6 +1,5 @@
 package Autocomplete;
 
-import java.util.*;
 
 public class Main{
 	public static void main(String[] args) {
--- src/Autocomplete/Node.java
+++ src/Autocomplete/Node.java
@@ -1,6 +1,5 @@
 package Autocomplete;
 
-import java.util.*;
 
 public class Node extends Tree{
 	protected Node[] parents = new Node[6]; 
