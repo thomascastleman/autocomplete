a8a1ac49488709fe23b4339aea251764fab90aab
Thomas Castleman <thomas7rex@gmail.com>
2017-08-12 20:10:26 -0400

    Cleaned up tree class, added new methods

--- src/Autocomplete/Tree.java
+++ src/Autocomplete/Tree.java
@@ -2,138 +2,50 @@ package Autocomplete;
 
 import java.util.*;
 
-
-
 public class Tree extends Main{
 	Node origin = null;
-	public ArrayList<ArrayList<Node>> layers = new ArrayList<ArrayList<Node>>();
-	public ArrayList<String> trainingData = new ArrayList<String>();
 	TreeType type;
 	
 	public Tree(TreeType t){
 		this.type = t;
+
+		/*
 		
-	}
-	public Tree(){
-		
-		
-	}
-/*
-	// search character tree for possible completions of a given string
-	public void charSearch(String s){
-	
-		Node n = origin;
-		boolean lowestNodeFound = false;
+		check for existing files of same TreeType
 
-		// for every character in string
-		for (int c = 0; c < s.length(); c++) {
-			// for every child of corresponding node for current character
-			for (int child = 0; child <= n.children.size(); child++) {
+		if (existing)
+			construct from file
+		else
+			format rawTrainingData
+			construct from training
+			write tree to file
 
-				// if no matching children found, break
-				if (child == n.children.size()) {
-					lowestNodeFound = true;
-					break;
-
-				// if child found, move to that node	
-//				} else if (((CharNode) n.children.get(child)).equals(s.charAt(c))) {	
-//					n = n.children.get(child);
-//					//break;
-//				}
-			}
-
-			if (lowestNodeFound) {
-				break;
-			}
-		}
-
-*/
-	// search for all completed children of lowest found node
-		//searchCompletedChildren(n);
-	
-
-	// depth first search from given node to find every completed-word child beneath it
-	public void searchCompletedChildren(Node n) {
-		for (int i = 0; i < n.children.size(); i++) {
-//			if (((CharNode) n.children.get(i)).isWord) {
-//				super.completionsFromCharSearch.add(n.children.get(i));
-//			}
-			searchCompletedChildren(n.children.get(i));
-		}
+		*/
 	}
 
-	// search word tree for possible completions of a given ngram
-	/*
-	public ArrayList<Node> wordSearch(String[] ngram){
-		Node n = origin;
-		boolean lowestNodeFound = false;
+	// // write tree structure to file
+	// public void writeToFile(String filename) {
 
-		// for every word in ngram
-		for (int word = 0; word < ngram.length; word++) {
-			// for every child of corresponding node for current character
-			for (int child = 0; child <= n.children.size(); child++) {
+	// }
 
-				// if no matching children found, break
-				if (child == n.children.size()) {
-					lowestNodeFound = true;
-					break;
+	// // construct tree from given file
+	// public void constructFromFile(String filename) {
 
-				// if child found, move to that node	
-				} else if (((WordNode) n.children.get(child)).equals(ngram[child])) {						
-					n = n.children.get(child);
-					break;
-				}
-			}
+	// }
 
-			if (lowestNodeFound) {
-				break;
-			}
-		}
+	// format raw training data into 2 dimensional string array (words for chartree, clauses and words for wordtree)
+	public String[][] formatData(String trainingData) {
 
-		// return all children of ngram
-		return n.children;
-	}
-*/
-
-	//@SuppressWarnings("static-access")
-	public void construct(Boolean isChar){
-		// check if tree files exist
-		// if yes, reconstruct from those
-		// else:
-
-		// character tree
-		if (isChar){
-		
-		// word tree
-		} else {
-			for (int word = 0; word < trainingData.size() - super.n; word++){
-				for(int gram = 0; gram < super.n; gram++){
-					System.out.print(trainingData.get(word + gram) + "-");
-				}
-				System.out.println("");
-			}
-		}
 	}
 
-	// write entire tree structure to txt file for later reconstruction
-	public void writeToFile() {
-		// open file here
+	// construct tree from formatted training data
+	public void constructFromTraining(String[][] formattedTraining) {
 
-		this.dfs(this.origin);
-
-		// close file
 	}
 
-	public void dfs(Node n) {
-		
-		for (int i = 0; i < n.children.size(); i++) {
-			
-			// Write (n.children.get(i).content + " " + n.children.get(i).probability + "\n") to file
-			
-			this.dfs(n.children.get(i));
-			
-			// Write "u" to file
-		}
+	// add a String[] section to the tree, updating what already exists and creating a new branch when necessary
+	public void addSection(String[] section) {
+
 	}
 }
 
